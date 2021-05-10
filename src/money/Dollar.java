5112e4c70af9d1b645869ed2d0368ebdd7cec216
10072002su_changping@cn.tre-inc.com <10072002su_changping@cn.tre-inc.com>
2021-05-10 16:26:05 +0900

    Initial commit

---
+++ src/money/Dollar.java
@@ -0,0 +1,19 @@
+package money;
+
+public class Dollar {
+
+    int amount;
+
+    public Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    public Dollar times(int multiplier) {
+        return new Dollar(amount * multiplier);
+    }
+
+    public boolean equals(Object object) {
+        Dollar dollar = (Dollar) object;
+        return amount == dollar.amount;
+    }
+}
